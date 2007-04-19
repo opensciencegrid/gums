6c369133a055b35ffe16f26b3f1b0688242f742c
Jay Packard <jpackard@bnl.gov>
2007-04-19 01:54:14 +0000

    r223 gums-client

---
+++ src/main/java/gov/bnl/gums/admin/PoolRemoveRange.java
@@ -0,0 +1,46 @@
+/*
+ * GUMS2MapUser.java
+ *
+ * Created on June 9, 2004, 1:44 PM
+ */
+package gov.bnl.gums.admin;
+
+import org.apache.commons.cli.*;
+
+/**
+ * @author carcassi
+ */
+public class PoolRemoveRange extends RemoteCommand {
+    static {
+        command = new PoolRemoveRange();
+    }
+
+    /**
+     * Creates a new ManualGroup_Add object.
+     */
+    public PoolRemoveRange() {
+        syntax = "ACCOUNTMAPPER RANGE";
+        description = "Removes range of accountsfrom a pool. " +
+            "ACCOUNTMAPPER is the name of the account mapper. " +
+            "RANGE is the group of accounts to be added (i.e. grid0050-125).";
+    }
+
+    protected org.apache.commons.cli.Options buildOptions() {
+        Options options = new Options();
+
+        return options;
+    }
+
+    protected void execute(org.apache.commons.cli.CommandLine cmd)
+        throws Exception {
+        if (cmd.getArgs().length < 2) {
+            failForWrongParameters("Missing parameters...");
+        }
+
+        String accountMapper = cmd.getArgs()[0];
+
+        for (int nArg = 2; nArg < cmd.getArgs().length; nArg++) {
+        	getGums().removeAccountRange(accountMapper, cmd.getArgs()[nArg]);
+        }
+    }
+}
