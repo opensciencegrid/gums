493182679e7dc8ac4df8fd12cd7de192dca792ad
Jay Packard <jpackard@bnl.gov>
2008-07-07 18:55:24 +0000

    r466 gums-core

--- src/main/java/gov/bnl/gums/account/LdapAccountMapper.java
+++ src/main/java/gov/bnl/gums/account/LdapAccountMapper.java
@@ -34,136 +34,136 @@ import org.apache.commons.logging.*;
  * @author Jay Packard
  */
 public class LdapAccountMapper extends AccountMapper {
-    static private Log log = LogFactory.getLog(LdapAccountMapper.class);
+	static private Log log = LogFactory.getLog(LdapAccountMapper.class);
 	static public String getTypeStatic() {
 		return "ldap";
 	}
-    private String jndiLdapUrl = "";
-    private String dnField = "description";
-    private String accountField = "uid";    
-    private String peopleObject = "ou=People";
+	private String jndiLdapUrl = "";
+	private String dnField = "description";
+	private String accountField = "uid";    
+	private String peopleObject = "ou=People";
 
-    public LdapAccountMapper() {
-    	super();
-    }
-    
-    public LdapAccountMapper(Configuration configuration) {
-    	super(configuration);
-    }
-    
-    public LdapAccountMapper(Configuration configuration, String name) {
-    	super(configuration, name);
-    }
-    
-    public AccountMapper clone(Configuration configuration) {
-    	LdapAccountMapper accountMapper = new LdapAccountMapper(configuration, new String(getName()));
-    	accountMapper.setDescription(new String(getDescription()));
-    	accountMapper.setJndiLdapUrl(new String(jndiLdapUrl));
-    	accountMapper.setDnField(new String(dnField));
-    	accountMapper.setAccountField(new String(accountField));
-    	accountMapper.setPeopleObject(new String(peopleObject));
-	return accountMapper;
-    }
-    
-    public String getAccountField() {
-        return accountField;
-    }
-    
-    public String getDnField() {
-        return dnField;
-    }
-    
-    public String getJndiLdapUrl() {
-        return jndiLdapUrl;
-    }
-    
-    public String getPeopleObject() {
-        return peopleObject;
-    }
+	public LdapAccountMapper() {
+		super();
+	}
 
-    public String getType() {
-	return "ldap";
-    }
-    
-    public String mapUser(String userDN, boolean createIfDoesNotExist) {
-    	Properties jndiProperties = retrieveJndiProperties();
-    	String userDNWithSubject = "subject="+userDN;
-        int nTries = 5;
-        int i = 0;
-        for (; i < nTries; i++) {
-            log.debug("Attempt " + i + " to retrieve map at '" + jndiLdapUrl + "'");
-            try {
-                DirContext jndiCtx = new InitialDirContext(jndiProperties);
-                NamingEnumeration nisMap = jndiCtx.search(peopleObject, "("+accountField+"=*)", null);
-                log.debug("Server responded");
-                while (nisMap.hasMore()) {
-                    SearchResult res = (SearchResult) nisMap.next();
-                    Attributes atts = res.getAttributes();
-                    Attribute dnAtt = atts.get(dnField);
-                    if (dnAtt != null) {
-                        String dn = dnAtt.get().toString();
-                        if (userDN.equals(dn) || userDNWithSubject.equals(dn)) {
-	                        String account = (String) atts.get(accountField).get();
-	                        log.debug("Found account '" + account + "' for DN '" + userDN + "'");
-	                        return account;
-                        }
-                    }
-                }
-                jndiCtx.close();
-            } catch (javax.naming.NamingException ne) {
-                log.warn("Error searching LDAP at "+jndiLdapUrl, ne);
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    log.warn("Interrupted", e);
-                }
-            } catch (Exception e) {
-                log.warn("Error searching LDAP at "+jndiLdapUrl, e);
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException ie) {
-                    log.warn("Interrupted", e);
-                }
-            }
-        }
-        return null;       
-    }
+	public LdapAccountMapper(Configuration configuration) {
+		super(configuration);
+	}
 
-    public void setAccountField(String accountField) {
-        this.accountField = accountField;
-    }
-    
-    public void setDnField(String dnField) {
-        this.dnField = dnField;
-    }
-    
-    public void setJndiLdapUrl(String jndiLdapUrl) {
-        this.jndiLdapUrl = jndiLdapUrl;
-    }
+	public LdapAccountMapper(Configuration configuration, String name) {
+		super(configuration, name);
+	}
 
-    public void setPeopleObject(String peopleObject) {
-        this.peopleObject = peopleObject;
-    }
+	public AccountMapper clone(Configuration configuration) {
+		LdapAccountMapper accountMapper = new LdapAccountMapper(configuration, new String(getName()));
+		accountMapper.setDescription(new String(getDescription()));
+		accountMapper.setJndiLdapUrl(new String(jndiLdapUrl));
+		accountMapper.setDnField(new String(dnField));
+		accountMapper.setAccountField(new String(accountField));
+		accountMapper.setPeopleObject(new String(peopleObject));
+		return accountMapper;
+	}
 
-    public String toString(String bgColor) {
-    	return "<td bgcolor=\""+bgColor+"\"><a href=\"accountMappers.jsp?command=edit&name=" + getName() + "\">" + getName() + "</a></td><td bgcolor=\""+bgColor+"\">" + getType() + "</td><td bgcolor=\""+bgColor+"\">&nbsp;</td>";
-    }
+	public String getAccountField() {
+		return accountField;
+	}
 
-    public String toXML() {
-    	return "\t\t<ldapAccountMapper\n"+
-			"\t\t\tname='"+getName()+"'\n"+
-			"\t\t\tdescription='"+getDescription()+"'\n"+
-			"\t\t\tjndiLdapUrl='"+jndiLdapUrl+"'\n"+
-			"\t\t\tdnField='"+dnField+"'\n"+
-			"\t\t\taccountField='"+accountField+"'\n"+
-			"\t\t\tpeopleObject='"+peopleObject+"'/>\n\n";
-    }
-    
-    private Properties retrieveJndiProperties() {
-        Properties jndiProperties = new java.util.Properties();
-        jndiProperties.put("java.naming.provider.url", jndiLdapUrl);
-        jndiProperties.put("java.naming.factory.initial","com.sun.jndi.ldap.LdapCtxFactory");
-        jndiProperties.put(Context.SECURITY_PROTOCOL, "none");
-        return jndiProperties;
-    } 
+	public String getDnField() {
+		return dnField;
+	}
+
+	public String getJndiLdapUrl() {
+		return jndiLdapUrl;
+	}
+
+	public String getPeopleObject() {
+		return peopleObject;
+	}
+
+	public String getType() {
+		return "ldap";
+	}
+
+	public String mapUser(String userDN, boolean createIfDoesNotExist) {
+		Properties jndiProperties = retrieveJndiProperties();
+		String userDNWithSubject = "subject="+userDN;
+		int nTries = 5;
+		int i = 0;
+		for (; i < nTries; i++) {
+			log.debug("Attempt " + i + " to retrieve map at '" + jndiLdapUrl + "'");
+			try {
+				DirContext jndiCtx = new InitialDirContext(jndiProperties);
+				NamingEnumeration nisMap = jndiCtx.search(peopleObject, "("+accountField+"=*)", null);
+				log.debug("Server responded");
+				while (nisMap.hasMore()) {
+					SearchResult res = (SearchResult) nisMap.next();
+					Attributes atts = res.getAttributes();
+					Attribute dnAtt = atts.get(dnField);
+					if (dnAtt != null) {
+						String dn = dnAtt.get().toString();
+						if (userDN.equals(dn) || userDNWithSubject.equals(dn)) {
+							String account = (String) atts.get(accountField).get();
+							log.debug("Found account '" + account + "' for DN '" + userDN + "'");
+							return account;
+						}
+					}
+				}
+				jndiCtx.close();
+			} catch (javax.naming.NamingException ne) {
+				log.warn("Error searching LDAP at "+jndiLdapUrl, ne);
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					log.warn("Interrupted", e);
+				}
+			} catch (Exception e) {
+				log.warn("Error searching LDAP at "+jndiLdapUrl, e);
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException ie) {
+					log.warn("Interrupted", e);
+				}
+			}
+		}
+		return null;       
+	}
+
+	public void setAccountField(String accountField) {
+		this.accountField = accountField;
+	}
+
+	public void setDnField(String dnField) {
+		this.dnField = dnField;
+	}
+
+	public void setJndiLdapUrl(String jndiLdapUrl) {
+		this.jndiLdapUrl = jndiLdapUrl;
+	}
+
+	public void setPeopleObject(String peopleObject) {
+		this.peopleObject = peopleObject;
+	}
+
+	public String toString(String bgColor) {
+		return "<td bgcolor=\""+bgColor+"\"><a href=\"accountMappers.jsp?command=edit&name=" + getName() + "\">" + getName() + "</a></td><td bgcolor=\""+bgColor+"\">" + getType() + "</td><td bgcolor=\""+bgColor+"\">&nbsp;</td>";
+	}
+
+	public String toXML() {
+		return "\t\t<ldapAccountMapper\n"+
+		"\t\t\tname='"+getName()+"'\n"+
+		"\t\t\tdescription='"+getDescription()+"'\n"+
+		"\t\t\tjndiLdapUrl='"+jndiLdapUrl+"'\n"+
+		"\t\t\tdnField='"+dnField+"'\n"+
+		"\t\t\taccountField='"+accountField+"'\n"+
+		"\t\t\tpeopleObject='"+peopleObject+"'/>\n\n";
+	}
+
+	private Properties retrieveJndiProperties() {
+		Properties jndiProperties = new java.util.Properties();
+		jndiProperties.put("java.naming.provider.url", jndiLdapUrl);
+		jndiProperties.put("java.naming.factory.initial","com.sun.jndi.ldap.LdapCtxFactory");
+		jndiProperties.put(Context.SECURITY_PROTOCOL, "none");
+		return jndiProperties;
+	} 
 }
