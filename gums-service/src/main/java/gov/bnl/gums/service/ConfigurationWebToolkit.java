2604baf3a81c768aaa8a9e54343f8d3d3d334ff7
John Hover <jhover@bnl.gov>
2012-11-10 22:00:51 +0000

    Require vogroup to be specified when matchfqan is 'vo'. https://jira.opensciencegrid.org/browse/GUMS-1
    r1182 branches/gums-1.3.x

--- gums-service/src/main/java/gov/bnl/gums/service/ConfigurationWebToolkit.java
+++ gums-service/src/main/java/gov/bnl/gums/service/ConfigurationWebToolkit.java
@@ -223,6 +223,13 @@ public class ConfigurationWebToolkit implements Remote {
 				((VOMSUserGroup)userGroup).setVoGroup( request.getParameter("vogroup") );
 			if (request.getParameter("role")!=null)
 				((VOMSUserGroup)userGroup).setRole( request.getParameter("role") );
+			// validate current state wrt matchFQAN and vogroup fields. 
+			// https://jira.opensciencegrid.org/browse/GUMS-1
+			if (request.getParameter("matchFQAN")!=null) && request.getParameter("matchFQAN").equals("vo"))
+			    if (request.getParameter("vogroup")!=null && request.getParameter("vogroup").equals(""))
+			    	throw new RuntimeException("If matchFQAN is 'vo' then 'vogroup' must be specified e.g., '/<voname>'.");
+			
+			
 		}
 
 		return userGroup;
