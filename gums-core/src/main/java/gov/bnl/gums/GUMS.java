007a621159820a50d6db2da21b576e6c1908f583
John Hover <jhover@bnl.gov>
2013-02-20 20:20:30 +0000

    r1200 branches/gums-1.3.x

--- gums-core/src/main/java/gov/bnl/gums/GUMS.java
+++ gums-core/src/main/java/gov/bnl/gums/GUMS.java
@@ -297,7 +297,8 @@ public class GUMS {
 					Class cl = this.gums.getCoreLogic().getClass();
 					//Method m = cl.getMethod(this.callMethod,  new Class[] {});
 					Method m = cl.getMethod(this.callMethod, null);
-					m.invoke(cl);
+					//m.invoke(cl);
+					m.invoke(cl, null);
 					gums.getCoreLogic().updateBannedGroups();
 					gumsAdminLog.info("Automatic updateBannedGroups ended");
 				} catch (Exception e) {
